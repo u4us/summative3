35792033e8848990d2b09149e32cabd218d114a6
Logan Pun <270021661@cit3-103m-05.acg.ac.nz>
2019-09-03 14:43:21 +1200

    added getProduct func.

--- niche-app/src/API.js
+++ niche-app/src/API.js
@@ -1,1 +1,17 @@
 import axios from 'axios';
+
+var urlPrefix = 'http://localhost:3001'
+
+var getProducts =() =>{
+    return axios.get(urlPrefix+'/products')
+}
+
+var getProduct =(id) =>{
+    return axios.get(urlPrefix+'products/'+id)
+}
+
+export {
+    getProducts,
+    getProduct,
+    
+}
